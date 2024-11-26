b87e05d06d125e77ba00caaeb1944db0c3459830
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-11-26 15:19:28 +0000

    Create GlobalExceptionHandler.java

---
+++ src/main/java/com/alucontrol/financeservice/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,56 @@
+package com.alucontrol.financeservice.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.UUID;
+import java.util.concurrent.TimeoutException;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    // Handle custom resource not found exception
+    @ExceptionHandler(ExpenseNotFoundException.class)
+    public ResponseEntity<ProblemDetails> handleExpenseNotFoundException(ExpenseNotFoundException ex) {
+
+        ProblemDetails problemDetails = new ProblemDetails(
+            HttpStatus.NOT_FOUND.toString(),
+            "Expense Not Found",
+            ex.getMessage(),
+            "A despesa solicitada não foi encontrada",
+                UUID.randomUUID()
+        );
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(problemDetails);
+    }
+
+    //Handling generic exceptions
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ProblemDetails> handleException(Exception ex){
+
+        ProblemDetails problemDetails = new ProblemDetails(
+                HttpStatus.INTERNAL_SERVER_ERROR.toString(),
+                "Internal Server Error",
+                "Ocorreu um erro inesperado no servidor",
+                ex.getMessage(),
+                UUID.randomUUID()
+        );
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(problemDetails);
+    }
+
+    //Deals with timeout of communication between microservices
+    @ExceptionHandler(TimeoutException.class)
+    public ResponseEntity<ProblemDetails> handleTimeoutException(TimeoutException ex){
+
+        ProblemDetails problemDetails = new ProblemDetails(
+                HttpStatus.REQUEST_TIMEOUT.toString(),
+                "Request Timeout",
+                "O tempo de resposta do microserviço de destino expirou",
+                ex.getMessage(),
+                UUID.randomUUID()
+        );
+        return ResponseEntity.status(HttpStatus.REQUEST_TIMEOUT).body(problemDetails);
+    }
+}
