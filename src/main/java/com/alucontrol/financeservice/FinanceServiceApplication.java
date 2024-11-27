16b6097863ca7b69f451ca57d22301d75a405579
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-11-27 13:37:29 +0000

    Update FinanceServiceApplication.java
    act:
    this is attempt to use the eureka services

--- src/main/java/com/alucontrol/financeservice/FinanceServiceApplication.java
+++ src/main/java/com/alucontrol/financeservice/FinanceServiceApplication.java
@@ -2,8 +2,10 @@ package com.alucontrol.financeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class FinanceServiceApplication {
 
     public static void main(String[] args) {
